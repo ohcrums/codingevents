0810d9d44d51887b3cfdfa5ac5c1a3595b9268bc
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-27 10:15:40 -0500

    tag functionality with DTO

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -5,7 +5,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
 import java.util.ArrayList;
 import java.util.List;
 
