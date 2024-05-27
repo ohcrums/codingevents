88458507df3647677f1f7c197b71b88a6362adfa
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-27 09:13:46 -0500

    relate Event to Tag

--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -1,8 +1,12 @@
 package org.launchcode.codingevents.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Tag extends AbstractEntity {
@@ -10,6 +14,9 @@ public class Tag extends AbstractEntity {
     @Size(min = 1, max = 25)
     @NotBlank
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private final List<Event> events = new ArrayList<>();
 
     public Tag(String name) {
         this.name = name;
@@ -29,4 +36,7 @@ public class Tag extends AbstractEntity {
         this.name = name;
     }
 
+    public List<Event> getEvents() {
+        return events;
+    }
 }
